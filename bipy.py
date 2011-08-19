43345c514d6e368f6bb02ac7ddea227ffdc7fbe0
Timothée Poisot <tpoisot@um2.fr>
2011-08-19 15:24:44 -0400

    Moved the functions for null models to their own file

--- bipy.py
+++ bipy.py
@@ -1,18 +1,20 @@
 ##########
 # bipy
+# main file
 ##########
 # A set of python functions to work on bipartite interaction networks
 # Timothee Poisot - Universite Montpellier 2
 # August 12, 2011
 # Contact : tpoisot@um2.fr
 ##########
-# Version using scipy / numpy
+# Most of the functions rely on
+# scipy & numpy
+#
 # Be sure to have them installed
 import scipy as sp
 import numpy as np
+from nullmodels import *
 ##########
-
-
 
 def fixmat(W):
 	# Fix a matrix so that there are no empty row
@@ -299,45 +301,6 @@ def qrange(V):
 	# Difference between maximal and minimal values
 	# of a vector
 	return max(V)-min(V)
-
-def nullC(ntop=30,nbottom=30,conn=0.5):
-	Nsize = ntop * nbottom
-	Wp = np.zeros((ntop,nbottom))
-	for i in range(ntop):
-		for j in range(nbottom):
-			IsInt = np.random.uniform(0,1,(1,))
-			if conn > IsInt:
-				Wp[i][j] = 1
-	return fixmat(Wp)
-
-def null1(W):
-	# Generate a random network based on the
-	# overall connectance of the web
-	C = connectance(W)
-	Wp = nullC(len(W),len(W[0]),C)
-	return fixmat(Wp)
-
-def null2(W):
-	# Generate a random network based on the
-	# probability that a row and a column
-	# have an interaction in the overall
-	# web
-	W = adjacency(W)
-	g = generality(W)
-	v = vulnerability(W)
-	for i in range(len(W)):
-		g[i] = g[i]/float(len(W[0]))
-	for j in range(len(W[0])):
-		v[j] = v[j]/float(len(W))
-	# Generate a random web
-	Wp = np.zeros((len(W),len(W[0])))
-	for i in range(len(W)):
-		for j in range(len(W[0])):
-			IsInt = np.random.uniform(0,1,(1,))
-			ProbInt = (g[i]+v[j])/2
-			if ProbInt > IsInt:
-				Wp[i][j] = 1
-	return fixmat(Wp)
 
 def d2h(V,bin='sturgis'):
 	# Returns the values needed to draw an histogram in PyX
