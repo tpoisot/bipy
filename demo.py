19512d38a31972dbfdde32810c65c05115c2c4b0
Timothée Poisot <tpoisot@um2.fr>
2011-08-19 17:53:59 -0400

    Added a pyx example to the demo.py file

--- demo.py
+++ demo.py
@@ -12,3 +12,19 @@ print "Nestedness (NODF)     : "+str(nodf(data)[0])+" "
 print ""
 print "The network looks like this :"
 prettyprint(nestadj(data))
+
+
+## EXAMPLE OF GRAPHICS
+## If you have pyx installed, uncomment the following : 
+#from pyx import *
+#
+#text.set(mode="latex")
+#text.preamble(r"\usepackage{fourier}")
+#
+#spedistrib = d2h(generality(data))
+#
+#g = graph.graphxy(width=8,
+#	x = graph.axis.linear(title='Degree',min=0,max=12),
+#	y = graph.axis.linear(title='Count',min=0,max=6))
+#g.plot(graph.data.points(spedistrib,x=1,y=2), [graph.style.histogram()])
+#g.writePDFfile("specificity")
