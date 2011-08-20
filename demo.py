dad5d1b94bf1f1d8226f7abf8be0a5bb413e7fe1
Timothée Poisot <tpoisot@um2.fr>
2011-08-20 15:22:47 -0400

    Added a "demo.web" file retrieved from IWDB and changed the demo file

--- demo.py
+++ demo.py
@@ -1,28 +1,31 @@
 from bipy import *
 
 print "Reading the network file..."
-data = readweb('wBL.web')
 
+print "Demo data were obtained from "
+print "Bangham, R. V. 1955. American Midland Naturalist 53:184-194."
 
-print "Some general infos..."
+print ""
+print ""
+
+data = readweb('demo.web').T
+
+print "Some general infos:"
 print "Connectance           : "+str(connectance(data))+" "
 print "Mean specificity      : "+str(mean(specificity(data)))+" "
 print "Nestedness (NODF)     : "+str(nodf(data)[0])+" "
 
 print ""
-print "The network looks like this :"
-prettyprint(nestadj(data))
-
 print ""
-print ""
-print "Using the bipartite class..."
-w = bipartite(readweb('wBL.web'))
 
-
-print "Some general infos..."
-print "Connectance           : "+str(w.connectance)+" "
-print "Mean specificity      : "+str(mean(w.specificity))+" "
-print "Nestedness (NODF)     : "+str(w.nodf)+" "
+w = bipartite(readweb('demo.web').T)
+print "Some general infos (using the bipartite class):"
+print "Connectance                   : "+str(w.connectance)+" "
+print "Mean specificity              : "+str(mean(w.specificity))+" "
+print "Mean number of hosts          : "+str(mean(w.generality))+" "
+print "Mean number of parasites      : "+str(mean(w.vulnerability))+" "
+print "Nestedness (NODF)             : "+str(w.nodf)+" "
+print "Network size                  : "+str(w.size)+" "
 
 ## EXAMPLE OF GRAPHICS
 ## If you have pyx installed, uncomment the following : 
