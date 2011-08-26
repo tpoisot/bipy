61028fe40b1937c6366b4054c25b4c0208789057
Timothée Poisot <tpoisot@um2.fr>
2011-08-26 14:21:04 -0400

    Confirmed that the p_findModules work on the EEC cluster

--- modularity.py
+++ modularity.py
@@ -7,19 +7,21 @@ w = bipartite(readweb('modular.web'),t=False)
 prettyprint(w.web)
 
 # Parallel
-
-start_time = time.time()
-print p_findModules(w,reps=10,ncpu=1)
-print "Time elapsed: ", time.time() - start_time, "s"
-
-start_time = time.time()
-print findModules(w,reps=10)
-print "Time elapsed: ", time.time() - start_time, "s"
+# Uncomment to test
+# Of course, you need to have a multi-core machine
+#start_time = time.time()
+#print p_findModules(w,reps=10,ncpu=1)
+#print "Time elapsed: ", time.time() - start_time, "s"
+# Tests with the demo data : 1000 repls in 2.3 sec
 
 print ""
 print "The modular network was hand-made"
 print "so that we expect 3 modules"
 print ""
+start_time = time.time()
+print findModules(w,reps=50)
+print "Time elapsed: ", time.time() - start_time, "s"
+
 #w = bipartite(readweb('demo.web'),t=True)
 #modulinfo = findModules(w,100,5) # Needs a number of replicates
 
