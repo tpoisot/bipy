1a0705cca92122d14b745ad427b7d3cc860614c7
Timothée Poisot <tpoisot@um2.fr>
2011-08-26 17:48:36 -0400

    Loads in SILENCE

--- bipy/__init__.py
+++ bipy/__init__.py
@@ -2,12 +2,6 @@
 
 __name__ = "bipy"
 
-print "BIPY (bipartite network analysis using python) loaded"
-print "---"
-print "Timothee Poisot - Universite Montpellier 2 - Contact : tpoisot@um2.fr"
-print "---"
-print "Loading functions"
-
 # Load the null models utilities
 from .gen import *
 from .nul import *
@@ -19,9 +13,3 @@ from .web import * # Web functions
 from mainfuncs import *
 from bipartite_class import *
 from getref import *
-
-
-print "Done, you're good to go!"
-print " "
-print " "
-print " "
