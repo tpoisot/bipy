e2ca122857191cbb3a7566cf8622b5114bd6a3ee
Timothée Poisot <tpoisot@um2.fr>
2011-09-08 00:34:36 +0200

    Temporary switched back to a version of nullModel ignoring the number of CPUs

--- bipy/nul/nullmodels.py
+++ bipy/nul/nullmodels.py
@@ -103,22 +103,14 @@ def p_null2(W,nreps=100,ncpu=2):
 
 ## Global wrapper around the null models
 def nullModel(W,null=1,nreps=1,ncpus=1):
-	gnreps= 30*nreps
-	if ncpus == 1:
-		out = []
-		if null == 1:
-			for i in range(gnreps):
-				tnmod = null1(W)
-				if (websize(tnmod) == W.size):
-					out.append(null1(W))
+	out = []
+	while len(out) < nreps:
+		if null == 1:		
+			tnmod = null1(W)
+			if (websize(tnmod) == W.size):
+				out.append(null1(W))
 		else:
-			for i in range(gnreps):
-				tnmod = null1(W)
-				if (websize(tnmod) == W.size):
-					out.append(null1(W))
-	else:
-		if null == 1:
-			out = p_null1(W,nreps=gnreps,ncpu=ncpus)
-		else:
-			out = p_null2(W,nreps=gnreps,ncpu=ncpus)
-	return out[:nreps]
+			tnmod = null2(W)
+			if (websize(tnmod) == W.size):
+				out.append(tnmod)
+	return out
