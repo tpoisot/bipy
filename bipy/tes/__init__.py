70615b76e038e34106d4edaacd5f4e1d2481ef7c
Timothée Poisot <tpoisot@um2.fr>
2011-10-12 12:46:10 +0200

    Working on extinction sequences function

--- bipy/tes/__init__.py
+++ bipy/tes/__init__.py
@@ -3,3 +3,4 @@
 __version__ = "1"
 
 from tests import *
+from extinct import *
---
+++ bipy/tes/extinct.py
@@ -0,0 +1,8 @@
+def extinctRobustness(w,method='random'):
+	#
+	# Method must be one of
+	# random : random removal
+	# stog : specialists to generalists
+	# gtos : generalists to specialists
+	#
+	return 0
