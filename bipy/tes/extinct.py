14538b1f0e3f26b8d826e6c7219957a75729e770
Timothée Poisot <tpoisot@um2.fr>
2011-10-12 12:49:29 +0200

    Extinct function

--- bipy/tes/extinct.py
+++ bipy/tes/extinct.py
@@ -1,8 +1,19 @@
-def extinctRobustness(w,method='random'):
+from .tests import *
+
+def extinctRobustness(w,method='random',removelower=True,tofile=False,nreps=100):
+	######
 	#
-	# Method must be one of
-	# random : random removal
-	# stog : specialists to generalists
-	# gtos : generalists to specialists
+	# method
+	# Must be one of
+	#	random : random removal
+	#	stog : specialists to generalists
+	#	gtos : generalists to specialists
 	#
+	# removelower
+	#	tells if the upper or lower level is removed
+	#
+	# nreps
+	#	Number of extinction sequences to perform
+	#
+	######
 	return 0
