e81e8a0f131a6818ded849a114da89a293d624b5
Timothée Poisot <tpoisot@um2.fr>
2011-11-20 19:07:48 -0500

    New null models (1, 2, 3rows, 3cols)

--- bipy/null/nullmodels.py
+++ bipy/null/nullmodels.py
@@ -2,74 +2,120 @@ import scipy as sp
 import numpy as np
 
 from ..mainfuncs import *
-from ..bipartite_class import *
 from ..base import *
 from ..spe import *
 
-## Null model C (needs the size and connectance)
-def nullC(ntop=30,nbottom=30,conn=0.5):
-	Nsize = ntop * nbottom
-	Wp = np.zeros((ntop,nbottom))
-	for i in range(ntop):
-		for j in range(nbottom):
-			IsInt = np.random.uniform(0,1,(1,))
-			if conn > IsInt:
-				Wp[i][j] = 1
-	return fixmat(Wp)
+def null_1(tem):
+	tint = np.prod(tem.shape)
+	zeroes = tint-np.sum(tem)
+	## We only return a matrix with no non-interacting species
+	hasNull = True
+	trials = 0
+	while hasNull:
+		trials += 1
+		mat = np.concatenate((np.ones(np.sum(tem)),np.zeros(zeroes)),1)
+		np.random.shuffle(mat)
+		mat = mat.reshape((len(tem),len(tem[0])))
+		NI0 = 0
+		NI1 = 0
+		for ms0 in mat.sum(axis=0):
+			if ms0 == 0:
+				NI0 = NI0 + 1
+		for ms1 in mat.sum(axis=1):
+			if ms1 == 0:
+				NI1 = NI1 + 1
+		if (NI0 == 0)&(NI1 == 0):
+			hasNull = False
+	return mat
 
 
-## Null model 1 (connectance based)
-def null1(W):
-	# Generate a random network based on the
-	# overall connectance of the web
-	Wp = nullC(W.upsp,W.losp,W.connectance)
-	fmw = fixmat(Wp)
-	if (len(fmw.shape) == 2):
-		if (fmw.shape[0] == W.upsp)&(fmw.shape[1] == W.losp):
-			fmw = mini_bipartite(fmw)
+def null_2(tem):
+	Ri = np.sum(tem,axis=1) / float(len(tem[0]))
+	Ci = np.sum(tem,axis=0) / float(len(tem))
+	hasNull = True
+	trials = 0
+	while hasNull:
+		trials += 1
+		mat = np.zeros(np.prod(tem.shape)).reshape((len(tem),len(tem[0])))
+		for row in range(len(tem)):
+				Pi = (Ri[row]+Ci)/float(2)
+				Ro = np.random.binomial(1,Pi)
+				mat[row] = Ro
+		NI0 = 0
+		NI1 = 0
+		for ms0 in mat.sum(axis=0):
+			if ms0 == 0:
+				NI0 = NI0 + 1
+		for ms1 in mat.sum(axis=1):
+			if ms1 == 0:
+				NI1 = NI1 + 1
+		if (NI0 == 0)&(NI1 == 0):
+			hasNull = False
+	return mat
+
+
+def null_3row(tem):
+	Ri = np.sum(tem,axis=1) / float(len(tem[0]))
+	hasNull = True
+	trials = 0
+	while hasNull:
+		trials += 1
+		mat = np.zeros(np.prod(tem.shape)).reshape((len(tem),len(tem[0])))
+		for row in range(len(tem)):
+				Ro = np.random.binomial(1,np.ones(len(tem[0]))*Ri[row])
+				mat[row] = Ro
+		NI0 = 0
+		NI1 = 0
+		for ms0 in mat.sum(axis=0):
+			if ms0 == 0:
+				NI0 = NI0 + 1
+		for ms1 in mat.sum(axis=1):
+			if ms1 == 0:
+				NI1 = NI1 + 1
+		if (NI0 == 0)&(NI1 == 0):
+			hasNull = False
+	return mat
+
+
+def null_3col(tem):
+	Ci = np.sum(tem,axis=0) / float(len(tem))
+	hasNull = True
+	trials = 0
+	while hasNull:
+		trials += 1
+		mat = np.zeros(np.prod(tem.shape)).reshape((len(tem),len(tem[0]))).T
+		for col in range(len(tem[0])):
+				Co = np.random.binomial(1,np.ones(len(tem))*Ci[col])
+				mat[col] = Co
+		mat = mat.T
+		NI0 = 0
+		NI1 = 0
+		for ms0 in mat.sum(axis=0):
+			if ms0 == 0:
+				NI0 = NI0 + 1
+		for ms1 in mat.sum(axis=1):
+			if ms1 == 0:
+				NI1 = NI1 + 1
+		if (NI0 == 0)&(NI1 == 0):
+			hasNull = False
+	return mat
+
+
+def nullModel(template,fun=null_1,replicates=1000,minconn=1.5):
+	ad = template.adjacency
+	nullWebs = []
+	# Check the level of constraints
+	numInt = np.sum(ad)
+	minInt = min(ad.shape)
+	rOrig = False
+	if numInt < minInt*minconn:
+		print 'WARNING: The connectance is low, you network is highly constrained'
+		print '         The original network will be returned'
+		rOrig = True
+	# Analyses
+	for i in range(replicates):
+		if(rOrig):
+			nullWebs.append(ad)
 		else:
-			fmw = np.zeros((0,0))
-	else:
-		fmw = np.zeros((0,0))
-	return fmw
-
-
-## Null model 2 (constrained)
-def null2(W):
-	# Generate a random network based on the
-	# probability that a row and a column
-	# have an interaction in the overall
-	# web
-	# Random matrix of probabilities
-	IsInt = np.random.uniform(0,1,(W.upsp,W.losp))
-	# Generate a random web
-	Wp = np.zeros((W.upsp,W.losp))
-	for i in range(W.upsp):
-		for j in range(W.losp):
-			ProbInt = (W.upsp*W.generality[i]+W.losp*W.vulnerability[j])/float(2*W.size)
-			if ProbInt > IsInt[i][j]:
-				Wp[i][j] = 1
-	fmw = fixmat(Wp)
-	if (len(fmw.shape) == 2):
-		if (fmw.shape[0] == W.upsp)&(fmw.shape[1] == W.losp):
-			fmw = bipartite(fmw)
-		else:
-			fmw = np.zeros((0,0))
-	else:
-		fmw = np.zeros((0,0))
-	return fmw
-
-
-## Global wrapper around the null models
-def nullModel(W,fun=null1,nreps=1,ncpus=1,maxiter=10000):
-	out = []
-	i = 0
-	while (len(out) < nreps)&(i<maxiter):
-		i += 1
-		## Do the null web
-		tnmod = fun(W)
-		## Check it
-		if hasattr(tnmod,'upsp'):
-			out.append(tnmod)
-	print str(len(out))+' null webs generated in '+str(i)+' iterations'
-	return out
+			nullWebs.append(fun(ad))
+	return nullWebs
--- bipy/tests/tests.py
+++ bipy/tests/tests.py
@@ -10,7 +10,7 @@ def getDevNest(w,list):
 		deviation.append(w.nodf-i.nodf)
 	return deviation
 
-	
+
 ## excess modularity
 def getDevMod(w,list):
 	m = [w.modules.Q,w.modules.N,w.modules.up_modules,w.modules.low_modules]
@@ -30,7 +30,6 @@ def getDevQ(w,list):
 		ExcQ = wQ - Qbip(i,w.modules.up_modules,w.modules.low_modules)
 		Qsim.append(ExcQ)
 	return Qsim
-
 
 
 ## remove a species from a matrix
@@ -61,44 +60,3 @@ def remSpecies(w,sp=0,fromTop=True,superMini=False):
 		mat = np.zeros((0,0))
 	# Returns a mini bipartite object
 	return mat
-
-
-# Get the contribution of each species to modularity
-def getContribMod(w,reps=1000):
-	# Get modularity
-	m = findModules(w,reps)
-	Qbip = m[0]
-	# Create two objects: up and low
-	CMup = []
-	CMlo = []
-	# Loop
-	for i in range(w.upsp):
-		tMat = remSpecies(w,i,True)
-		rQbip = findModules(tMat,reps)[0]
-		tStat = np.abs(rQbip-Qbip)
-		CMup.append(tStat)
-	for i in range(w.losp):
-		tMat = remSpecies(w,i,False)
-		rQbip = findModules(tMat,reps)[0]
-		tStat = np.abs(rQbip-Qbip)
-		CMlo.append(tStat)
-	# Return the data
-	return [CMup,CMlo]
-
-
-# Get the contribution of each species to nestedness
-def getContribNes(w):
-	# Create two objects: up and low
-	CNup = []
-	CNlo = []
-	# Loop
-	for i in range(w.upsp):
-		tMat = remSpecies(w,i,True)
-		tStat = np.abs(tMat.nodf-w.nodf)
-		CNup.append(tStat)
-	for i in range(w.losp):
-		tMat = remSpecies(w,i,False)
-		tStat = np.abs(tMat.nodf-w.nodf)
-		CNlo.append(tStat)
-	# Return the data
-	return [CNup,CNlo]
