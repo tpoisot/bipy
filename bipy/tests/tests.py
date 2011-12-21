420c2dcbfacadc66088400916cc98c139a5717d1
Tim Poisot <timothee.poisot@uqar.ca>
2011-12-21 11:11:21 -0500

    Changed the test functions, added a test class and optimized modularity testing // still in progress

--- bipy/tests/tests.py
+++ bipy/tests/tests.py
@@ -3,87 +3,90 @@
 from ..nes import *
 from ..mod import *
 from ..bipartite_class import *
-
 import scipy.stats as spp
 
+class test:
+    def __init__(self,web,model,replicates,verbose):
+        """
+        Initialize a test class and perform the tests
+        """
+        self.v = verbose
+        self.web = web
+        if self.v:
+            print "Creation of the null networks"
+        self.nulls = nullModel(self.web,model,replicates=replicates)
+        if self.v:
+            print "Tests"
+        self.devnest = []
+        self.devqb = []
+        self.devqr = []
+    def nestedness(self):
+        if self.v:
+            print "Begin testing nestedness"
+        self.devnest = getDevNest(self.web,self.nulls)
+        if self.v:
+            print "Nestedness test completed"
+    def modularity(self,repl):
+        d_mod = getDevMod(self.web,self.nulls,repl)
+        self.devqr = d_mod[0]
+        self.devqb = d_mod[1]
+    def __str__(self):
+        out = "Stat\tN0\t\tN'\t\tp\t\tIC-\t\tIC+\n"
+        out +=  "-----------------------------------------\n"
+        if len(self.devnest) > 0:
+            p_nest = '---'
+            if self.devnest[1] < 0.05:
+                p_nest = '*  '
+            elif self.devnest[1] < 0.001:
+                p_nest = '** '
+            elif self.devnest[1] < 0.00001:
+                p_nest = '***'
+            out += "NODF\t"+str(round(self.devnest[0],2))+"\t"+str(round(self.devnest[2],2))+"\t"+p_nest+"\t"+str(round(self.devnest[3],2))+"\t"+str(round(self.devnest[4],2))
+        return out
+
 def gMIC(distrib):
-	estimates = spp.bayes_mvs(distrib,alpha=0.95)[0]
-	return[estimates[0],estimates[1][0],estimates[1][1]]
+    """
+    Bayesian estimates of mean, and standard deviation
+    """
+    estimates = spp.bayes_mvs(distrib,alpha=0.95)[0]
+    return[estimates[0],estimates[1][0],estimates[1][1]]
 
-## excess modularity
+## excess nestedness
 def getDevNest(w,list):
-	expect = []
-	for i in list:
-		expect.append(bipartite(i).nodf)
-	testRes = stats.ttest_1samp(expect, w.nodf)
-	OUT = [w.nodf,testRes[1]]
-	est = gMIC(expect)
-	for est_par in est:
-		OUT.append(est_par)
-	return OUT
-
+    expect = []
+    for i in list:
+        expect.append(bipartite(i).nodf)
+    testRes = stats.ttest_1samp(expect, w.nodf)
+    OUT = [w.nodf,testRes[1]]
+    est = gMIC(expect)
+    for est_par in est:
+        OUT.append(est_par)
+    return OUT
 
 ## excess modularity
-def getDevQr(w,list):
-	if w.modules.N == 1:
-		return [0,0,0,0,0]
-	m = [w.modules.Q,w.modules.N,w.modules.up_modules,w.modules.low_modules]
-	Qsim = []
-	wQr = Qr(w,m)
-	for i in list:
-		Qsim.append(Qr(bipartite(i),m))
-	testRes = stats.ttest_1samp(Qsim, wQr)
-	OUT = [wQr,testRes[1]]
-	est = gMIC(Qsim)
-	for est_par in est:
-		OUT.append(est_par)
-	return OUT
-
-
-## excess bipartite modularity
-def getDevQbip(w,list):
-	if w.modules.N == 1:
-		return [0,0,0,0,0]
-	Qsim = []
-	wQ = w.modules.Q
-	for i in list:
-		Qsim.append(Qbip(bipartite(i),w.modules.up_modules,w.modules.low_modules))
-	testRes = stats.ttest_1samp(Qsim, wQ)
-	OUT = [wQ,testRes[1]]
-	est = gMIC(Qsim)
-	for est_par in est:
-		OUT.append(est_par)
-	return OUT
-
-
-## excess modularity Prime
-def getDevQrALL(w,list):
-	m = [w.modules.Q,w.modules.N,w.modules.up_modules,w.modules.low_modules]
-	Qsim = []
-	wQr = Qr(w,m)
-	for i in list:
-		mod = modules(bipartite(i),reps=100)
-		Qsim.append(mod.Qr)
-	testRes = stats.ttest_1samp(Qsim, wQr)
-	OUT = [wQr,testRes[1]]
-	est = gMIC(Qsim)
-	for est_par in est:
-		OUT.append(est_par)
-	return OUT
-
-
-## excess bipartite modularity Prime
-def getDevQbipALL(w,list):
-	if w.modules.N == 1:
-		return [0,0,0,0,0]
-	Qsim = []
-	wQ = w.modules.Q
-	for i in list:
-		mod = modules(bipartite(i),reps=100)
-		Qsim.append(mod.Q)
-	testRes = stats.ttest_1samp(Qsim, wQ)
-	OUT = [wQ,testRes[1]]
-	est = gMIC(Qsim)
-	for est_par in est:
-		OUT.append(est_par)
-	return OUT
+def getDevMod(w,nulls,rep):
+    """
+    Get the deviation from random expectation of modularity. Optimized so that
+    the null webs are gone through only one time. Retunrs two arrays, one for
+    Qr, the other for Qb.
+    """
+    m = [w.modules.Q,w.modules.N,w.modules.up_modules,w.modules.low_modules]
+    Qbsim = []
+    Qrsim = []
+    wQr = Qr(w,m)
+    wQb = w.modules.Q
+    for c_null in nulls:
+        mod = modules(bipartite(c_null),reps=rep)
+        Qrsim.append(mod.Qr)
+        Qbsim.append(mod.Q)
+    testResB = stats.ttest_1samp(Qbsim, wQb)
+    testResR = stats.ttest_1samp(Qrsim, wQr)
+    OUT_r = [wQr,testResR[1]]
+    OUT_b = [wQr,testResB[1]]
+    est_r = gMIC(Qrsim)
+    est_b = gMIC(Qbsim)
+    for est_par in est_r:
+        OUT_r.append(est_par)
+    for est_par in est_b:
+        OUT_b.append(est_par)
+    return [OUT_r,OUT_b]
--- modularity.py
+++ modularity.py
@@ -22,12 +22,17 @@ start_time = time.time()
 w.modules = modules(w,reps=10)
 print "Time elapsed: ", time.time() - start_time, "s"
 
-print 'Null 2'
-n2 = nullModel(w,null_2,replicates=300)
+test_web = test(w,null_2,100,verbose=False)
+test_web.nestedness()
+print test_web.devnest
+print test_web
 
-print 'Test for nestedness'
-print getDevNest(w,n2)
-print 'Test for QR'
-print getDevQr(w,n2)
-print 'Test for Q bip'
-print getDevQbip(w,n2)
+#print 'Null 2'
+#n2 = nullModel(w,null_2,replicates=300)
+#import cProfile
+#print 'Test for nestedness'
+#print cProfile.run('getDevNest(w,n2)')
+#print 'Test for QR'
+#print getDevQr(w,n2)
+#print 'Test for Q bip'
+#print getDevQbip(w,n2)
